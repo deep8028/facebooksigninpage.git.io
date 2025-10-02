041c7a64ea49e611a5531ad712e1987451ca8b26
deep8028 <deepa2880lakshmi@gmail.com>
2025-10-02 13:52:40 +0530

    Add files via upload

---
+++ validation.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+ $('#signup').validate({
+    rules: {
+        fname: {
+            required: true,
+            minlength:5,
+        },
+        sname:{
+            required: true,
+            minlength:5,
+        },
+        mobile:{
+            required: true,
+            minlength:10,
+        },
+        password:{
+            required: true,
+            minlength:8,
+        },
+        day:{
+            required:true,
+        },
+        month:
+        {
+            required:true,
+        },
+        year:{
+            required:true,
+        },
+        gender:{
+            required:true,
+        },
+        gender:{
+            required:true,
+        },
+    }
+ }
+ )
+});
