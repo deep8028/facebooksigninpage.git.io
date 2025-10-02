a52a1c97a9b90c35eb7ab4c7fb00c8712ce33287
deep8028 <deepa2880lakshmi@gmail.com>
2025-10-02 13:54:52 +0530

    Update validation.js

--- validation.js
+++ validation.js
@@ -1,39 +1,44 @@
 $(document).ready(function() {
- $('#signup').validate({
+  $('#signup').validate({
     rules: {
-        fname: {
-            required: true,
-            minlength:5,
-        },
-        sname:{
-            required: true,
-            minlength:5,
-        },
-        mobile:{
-            required: true,
-            minlength:10,
-        },
-        password:{
-            required: true,
-            minlength:8,
-        },
-        day:{
-            required:true,
-        },
-        month:
-        {
-            required:true,
-        },
-        year:{
-            required:true,
-        },
-        gender:{
-            required:true,
-        },
-        gender:{
-            required:true,
-        },
+      fname: {
+        required: true,
+        minlength: 2
+      },
+      sname: {
+        required: true,
+        minlength: 2
+      },
+      mobile: {
+        required: true,
+        minlength: 10
+      },
+      password: {
+        required: true,
+        minlength: 8
+      },
+      Date: {   // fixed (was "day")
+        required: true
+      },
+      months: { // fixed (was "month")
+        required: true
+      },
+      year: {
+        required: true
+      },
+      gender: {
+        required: true
+      }
+    },
+    messages: {
+      fname: "Please enter your first name",
+      sname: "Please enter your surname",
+      mobile: "Enter a valid mobile number or email",
+      password: "Password must be at least 8 characters",
+      Date: "Select date",
+      months: "Select month",
+      year: "Select year",
+      gender: "Please select gender"
     }
- }
- )
+  });
 });
